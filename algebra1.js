91b5310f9855b6c56a589810751e39870b11cfa7
Inzimam <p126013@nu.edu.pk>
2017-05-03 00:48:17 +0500

    Algebra simple function

---
+++ algebra1.js
@@ -0,0 +1,13 @@
+// Create a function to check if a number is less than or equal to zero. Return true or false.
+// Examples
+// 5 => false
+// 0 => true
+// -2 => true
+
+function lessThanOrEqualToZero(num) {
+var num;
+  if(num > 0){
+    return false;
+  }
+  return true;
+}
