b92722da2dd401763166b432c1df8f8cf871a4c0
Inzimam <p126013@nu.edu.pk>
2017-05-06 17:20:12 +0500

    Return The Last Item In An Array

---
+++ arrays1.js
@@ -0,0 +1,15 @@
+// Create a function that accepts an array and returns the last item in the array. The array can contain any of JavaScript's five primitive data types.
+// Number
+// String
+// Boolean
+// Undefined
+// Null
+// Examples
+
+// [1, 2, 3] => 3
+// ['cat', 'dog', 'duck'] => 'duck'
+// [true, false, true] => true
+
+function getLastItem(arr) {
+    return arr[arr.length - 1];
+}
