23b63e2e95d13874549759fb96fd4c7537c70180
Inzimam <p126013@nu.edu.pk>
2017-05-17 19:51:18 +0500

    Find The Minimum, Maximum, Length And Average Values

---
+++ Easy/arrays5.js
@@ -0,0 +1,19 @@
+// Create a function that takes an array of numbers and returns the  following statistics:
+// Minimum Value
+// Maximum Value
+// Sequence Length
+// Average Value
+// Examples:
+// [6, 9, 15, -2, 92, 11] => [-2, 92, 6, 21.833333333333332]
+// [30, 43, 20, 92, 3, 74] => [3, 92, 6, 43.666666666666664]
+// [4.54, 8.32, 5.20] => [4.54, 8.32, 3, 6.02]
+
+// Find The Minimum, Maximum, Length And Average Values
+
+function minMaxLengthAverage(arr) {
+    return [Math.min(...arr),
+        Math.max(...arr),
+        arr.length,
+        arr.reduce((a, b) => a + b) / arr.length
+    ];
+}
